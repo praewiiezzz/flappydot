1b97665693360c841fa8444add05777cdb18b106
project1 <proj@test.com>
2014-09-08 19:31:39 +0700

    Add ความเร็วของ ท่อ ให้ท่อเคลื่อนที่ได้

--- src/flappydot/PillarPair.java
+++ src/flappydot/PillarPair.java
@@ -8,13 +8,15 @@ public class PillarPair {
 	private Image bottomPillar;
 	private float x;
 	private float y;
+	private float vx;
 	static public final int WIDTH = 80;
 	static public final int IMAGE_HEIGHT = 600;
 	static public final int PILLAR_SPACE = 200;
 
-	public PillarPair(float x, float y) throws SlickException {
+	public PillarPair(float x, float y,float vx) throws SlickException {
 		this.x = x;
 		this.y = y;
+		this.vx = vx;
 		topPillar = new Image("res/pillar-top.png");
 		bottomPillar = new Image("res/pillar-bottom.png");
 	}
@@ -22,5 +24,9 @@ public class PillarPair {
 	public void render() {
 		topPillar.draw(x-(WIDTH/2),FlappyDotGame.GAME_HEIGHT -(y + IMAGE_HEIGHT+(PILLAR_SPACE/2)));
 		bottomPillar.draw(x-(WIDTH/2),FlappyDotGame.GAME_HEIGHT-(y-(PILLAR_SPACE/2)));
+	}
+	
+	public void update() {
+		x +=vx;
 	}
 }
